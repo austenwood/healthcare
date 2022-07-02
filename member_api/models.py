4696654f4b76b0362b224ede5e99fd7f1a0ac91b
Austen Wood <50690066+austenwood@users.noreply.github.com>
2022-07-02 14:34:47 -0700

    Change phone_number field to use models.IntegerField()

--- member_api/models.py
+++ member_api/models.py
@@ -1,17 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
-from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Member(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    phone_number = PhoneNumberField()
+    phone_number = models.IntegerField()
     client_member_id = models.IntegerField()
     account_id = models.IntegerField()
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
 
 class Provider(models.Model):
     account_id = models.IntegerField()
