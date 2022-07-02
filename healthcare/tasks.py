b5deb1a343765e4f611474044547a1766ee3677f
Austen Wood <50690066+austenwood@users.noreply.github.com>
2022-07-02 14:41:39 -0700

    Add batch file upload view and celery task

---
+++ healthcare/tasks.py
@@ -0,0 +1,25 @@
+from member_api.serializers import MemberSerializer
+from .celery import app
+from member_api.models import Member
+import pandas as pd
+
+
+@app.task()
+def process_member_data(chunk):
+    df = pd.read_json(chunk)
+    row_iter = df.iterrows()
+
+    member_data = []
+    for index, row in row_iter:
+        data = {
+            'first_name': row['first_name'],
+            'last_name': row['last_name'],
+            'phone_number': row['phone_number'],
+            'client_member_id': row['client_member_id'],
+            'account_id': row['account_id']
+        }
+        serializer = MemberSerializer(data=data)
+        if serializer.is_valid():
+            member_data.append(Member(**serializer.validated_data))
+
+    Member.objects.bulk_create(member_data)
--- member_api/views.py
+++ member_api/views.py
@@ -1,18 +1,22 @@
-from rest_framework.response import Response
 from rest_framework import status, permissions, generics
+from rest_framework.response import Response
 from .models import Member
-from .serializers import MemberSerializer
+from .serializers import MemberSerializer, FileUploadSerializer
+from healthcare.tasks import process_member_data
+import pandas as pd
 
 
 class MemberListApiView(generics.ListAPIView):
     # check if user is authenticated
     permission_classes = [permissions.IsAuthenticated]
 
-    serializer_class = MemberSerializer
     queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+    
+    # fields to allow filtering against
     filterset_fields = ['account_id', 'id', 'phone_number', 'client_member_id']
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         data = {
             'first_name': request.data.get('first_name'),
             'last_name': request.data.get('last_name'),
@@ -28,3 +32,24 @@ class MemberListApiView(generics.ListAPIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UploadFileView(generics.CreateAPIView):
+    # check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    serializer_class = FileUploadSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        file = serializer.validated_data['file']
+
+        chunk_size = 1000
+        with pd.read_csv(file, chunksize=chunk_size) as reader:
+            for chunk in reader:
+                serialized_chunk = chunk.to_json()
+                process_member_data.delay(serialized_chunk)
+
+        return Response({'status': 'success'}, status.HTTP_201_CREATED)
