a8d90623bc7e118d4edb8a7c240321052919f0e1
Austen Wood <50690066+austenwood@users.noreply.github.com>
2022-07-02 14:43:44 -0700

    Add upload path url

--- member_api/urls.py
+++ member_api/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from .views import (
     MemberListApiView,
+    UploadFileView
 )
 
 urlpatterns = [
     path('members/', MemberListApiView.as_view()),
+    path('upload/', UploadFileView.as_view(), name='upload-file')
 ]
