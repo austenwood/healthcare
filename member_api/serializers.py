aec4688601fd61b74b4ded7dfa463db78fc52c42
Austen Wood <50690066+austenwood@users.noreply.github.com>
2022-07-02 14:43:04 -0700

    Add unique together validators for constraints

--- member_api/serializers.py
+++ member_api/serializers.py
@@ -1,6 +1,24 @@
 from rest_framework import serializers
 from .models import Member
+from rest_framework.validators import UniqueTogetherValidator
+
+
 class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
-        fields = ['first_name', 'last_name', 'phone_number', 'client_member_id', 'account_id']
+        fields = ['first_name', 'last_name', 'phone_number',
+                  'client_member_id', 'account_id']
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Member.objects.all(),
+                fields=['phone_number', 'account_id']
+            ),
+            UniqueTogetherValidator(
+                queryset=Member.objects.all(),
+                fields=['phone_number', 'account_id']
+            )
+        ]
+
+
+class FileUploadSerializer(serializers.Serializer):
+    file = serializers.FileField()
