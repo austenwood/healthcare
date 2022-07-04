6ad424481a9a5114db103a1cc5dea841c354ab8a
Austen Wood <50690066+austenwood@users.noreply.github.com>
2022-07-03 17:30:06 -0700

    Change phone number to use models.CharField()

--- healthcare/member_api/models.py
+++ healthcare/member_api/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Member(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    phone_number = models.IntegerField()
+    phone_number = models.CharField(max_length=100)
     client_member_id = models.IntegerField()
     account_id = models.IntegerField()
