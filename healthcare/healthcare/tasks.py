d96e123716d90847de51ff8392004eb086610006
Austen Wood <50690066+austenwood@users.noreply.github.com>
2022-07-04 00:11:10 -0700

    Remove validation in process_member_data()

--- healthcare/healthcare/tasks.py
+++ healthcare/healthcare/tasks.py
@@ -11,17 +11,14 @@ def process_member_data(chunk):
     df = pd.read_json(chunk)
     row_iter = df.iterrows()
 
-    member_data = []
-    for index, row in row_iter:
-        data = {
-            'first_name': row['first_name'],
-            'last_name': row['last_name'],
-            'phone_number': row['phone_number'],
-            'client_member_id': row['client_member_id'],
-            'account_id': row['account_id']
-        }
-        serializer = MemberSerializer(data=data)
-        if serializer.is_valid():
-            member_data.append(Member(**serializer.validated_data))
+    member_data = [
+        Member(
+            first_name=row['first_name'],
+            last_name=row['last_name'],
+            phone_number=row['phone_number'],
+            client_member_id=row['client_member_id'],
+            account_id=row['account_id']
+        ) for i, row in row_iter
+    ]
 
     Member.objects.bulk_create(member_data)
